49fab1c5bf840bdb8e95f3e301ec64ac4934e971
nguetcheu <nguetcheukuinsidominique@gmail.com>
2021-09-30 23:32:04 +0100

    creation des boutons

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,33 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(
-        title: Text('My First App'),
+    var questions = [
+      "Quel est votre couleur favorite?"
+      "Quel est votre animal favori?"
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text('la question'),
+            ElevatedButton(
+              onPressed: null, 
+              child: Text("Reponse 1"),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text("Reponse 2"),
+            ),
+            ElevatedButton(
+              onPressed: null, 
+              child: Text("Reponse 3"),
+            ),
+          ],
+        ),
       ),
-      body: Text('Ceci est le texte par défaut'),
-    ),
     );
   }
 }
